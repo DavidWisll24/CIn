8b8fd9517da7a7056832dc9b4e70605ddb2c7a67
Davidson Wisllety <169708334+DavidWisll24@users.noreply.github.com>
2026-04-07 10:00:54 -0300

    Nova lista - Listas

---
+++ CIn/IP/Lista 3/Exercicio_1.md
@@ -0,0 +1,16 @@
+# Sistema para embarque, desembarque e movimentação na Coração de Ouro
+
+## Tripulação inicial
+- Zaphod Beeblebrox (o presidente da galáxia de duas cabeças)
+- Ford Prefect (um escritor do Guia do Mochileiro das Galáxias)
+- Arthur Dent (um terráqueo desorientado)
+- Marvin (um robô depressivo)
+
+## Eventos possíveis
+**Embarcar**: Alguem entra na nave no fim da lista de tripulantes
+**Priorizar**: O tripulante precisa ir para o inicio da lista
+**Remover**: O tripulante sai da nave
+**Mover**: O tripulante muda de local na fila
+
+O sistema recebe o nome primeiro o número de ocorrências(valor int). Depois, pede a ação(evento) a ser realizada, o tripulante que sofrerá essa ação e, caso seja "Mover", o index que ele ocupará (valor str).
+
---
+++ CIn/IP/Lista 3/Exercicio_1.py
@@ -0,0 +1,74 @@
+#Verificando quantos ocorrências vão surgir
+num_ocorrencias = int(input())
+
+print("Édipo: Inicializando sistema de embarque. Tripulantes atuais: Zaphod Beeblebrox, Ford Prefect, Arthur Dent, Marvin")
+
+#Definindo os primeiros tripulantes
+tripulantes = ["Zaphod Beeblebrox", "Ford Prefect", "Arthur Dent", "Marvin"]
+
+for _ in range(num_ocorrencias):
+    #Qual o evento dá vez
+    ocorencia = input()
+
+    #Transformando string em uma lista
+    evento = ocorencia.split()
+
+    if "mover" in evento:
+        atual_tripulante = " ".join(evento[1:-1:1])
+    else:
+        atual_tripulante = " ".join(evento[1::1])
+    
+    #Ordenando a tripulação
+    if evento[0] == 'embarcar':
+        tripulantes.append(atual_tripulante)
+
+    elif evento[0] == 'mover':
+        if evento[1] in tripulantes:
+            index_tripulante = tripulantes.index(atual_tripulante)
+            tripulante_removido = tripulantes.pop(index_tripulante)
+            tripulantes.insert(evento[-1], tripulante_removido)
+
+    elif evento[0] == 'remover':
+        if atual_tripulante in tripulantes:
+            tripulantes.remove(atual_tripulante)
+
+    elif evento[0] == 'priorizar':
+        if atual_tripulante in tripulantes:
+            tripulantes.insert(0, atual_tripulante)
+
+    #Prints especiais
+    if ( (evento[0] == "mover" and "Zaphod Beeblebrox" in ocorencia and evento[-1] == "0") 
+        or (evento[0] == "priorizar" and "Zaphod Beeblebrox" in ocorencia)
+    ):
+        print("EU SOU O PRESIDENTE DA GALAXIA! Primeiro lugar é pouco!")
+
+    elif ( (evento[0] == "mover" and "Ford Prefect" in ocorencia and evento[-1] == "0") 
+        or (evento[0] == "priorizar" and "Ford Prefect" in ocorencia)
+    ):
+        print("Sou um escritor do Guia! Mereço destaque!")
+
+    elif evento[0] == "remover" and "Marvin" in ocorencia:
+        print("Ninguem se importa comigo mesmo. Tchau")
+
+    elif evento[0] == "remover" and "Arthur Dent" in ocorencia:
+        print("Eu só queria poder tomar chá... vou descer no próximo planeta")
+
+    elif evento[0] == "embarcar" and "Trillian" in ocorencia:
+        print("Finalmente alguém sensata a bordo! Bem-vinda, Trillian!")
+
+#FIM do programa
+print(f"Édipo: Graças à improbabilidade, os novos comandantes são: {tripulantes[0]}, {tripulantes[1]} e {tripulantes[2]}.")
+
+
+if tripulantes[3::1] != []:
+    print("Convocando tripulantes:")
+    for convocado in tripulantes[3::1]:
+        print(f"- {convocado}")
+
+elif len(tripulantes) >= 2 and len(tripulantes) != 0:
+    print("Convocando tripulantes:")
+    for convocado in tripulantes:
+        print(f"- {convocado}")
+
+else:
+    print("Édipo: Graças à improbabilidade, os novos comandantes são: ninguém... a nave está vazia!")
