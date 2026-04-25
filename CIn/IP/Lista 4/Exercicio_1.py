8d71007397fa1689855435edd9193a097b0eb9f3
Davidson Wisllety <169708334+DavidWisll24@users.noreply.github.com>
2026-04-25 10:15:14 -0300

    Questão 1 Finalizada

--- CIn/IP/Lista 4/Exercicio_1.py
+++ CIn/IP/Lista 4/Exercicio_1.py
@@ -16,95 +16,93 @@ else:
 def actions_hornet(action): #* Função Hornet 
     if action == "Ferrão":
         dano_hornet = 10
-        seda = 2
-        return dano_hornet, seda
+        sistem_carretel[0] += 2 #* Pondo no carretel
+        sistem_carretel[1] += 2 #* Pondo no tatal de seda feita
+        return dano_hornet
     elif action == "Ataque de Seda":
-        dano_hornet = 10
-        seda = -3
-        return dano_hornet, seda
+        if sistem_carretel[0] >= 3:
+            dano_hornet = 20
+            sistem_carretel[0] -= 3 #* Tirando do carretel
+            sistem_carretel[2] += 3 #* Somando na seda gasta
+            return dano_hornet
+        else:
+            return 0
     elif action == "Vincular":
-        vida_curada = 3
-        seda = -8
-        return vida_curada, seda
-    
+        if sistem_carretel[0] == 8:
+            vida_curada = 3
+            sistem_carretel[0] -= 8 #* Tirando do carretel
+            sistem_carretel[2] += 8 #* Somando na seda gasta
+            return vida_curada
+        else:
+            return 0
+        
 def actions_boss(reaction): #* Função do Chefe
     if reaction == "Acerto":
         dano_chefe = 1
         return dano_chefe
-    elif reaction == "Duplo Acerto":
+    elif reaction == "Acerto Duplo":
         dano_chefe = 2
         return dano_chefe
     elif reaction == "Errou":
-        esquiva = True
-        return esquiva
+        return 0
     
-def sistem_battle(acao, mascaras_recuperadas, mask_hornet, carretel, seda_recuperada, seda_desperdicada, vida_chefe, total_sedas_recuperadas, total_sedas_utilizadas): #* Função Combate
+def sistem_battle(acao, vida_chefe): #* Função Combate
     ##Status Da Hornet
     
     ##Averiguando ação
     if acao == "Ferrão":
-        dano_causado, seda_recuperada = actions_hornet(acao) #*Pegando os valores da função
+        dano_causado = actions_hornet(acao) #*Pegando os valores da função
 
         vida_chefe -= dano_causado #*Causando dano no chefe
-        carretel += seda_recuperada #*Pegando seda
-        if carretel > 8: #*Garante que não passe de 8 sedas
-            carretel = 8
-
-        total_sedas_recuperadas += seda_recuperada #*Armazena o total de sedas geradas
+        if sistem_carretel[0] > 8: #*Garante que não passe de 8 sedas
+            sistem_carretel[0] = 8
 
     elif acao == "Ataque de Seda":
-        dano_causado, seda_usada = actions_hornet(acao)
-
-        if carretel >= 3: #*Se tiver Seda o Suficiente
-            carretel += seda_usada
-            total_sedas_utilizadas += seda_usada
-            vida_chefe -= dano_causado
+        dano_causado = actions_hornet(acao)
+        vida_chefe -= dano_causado
 
     elif acao == "Vincular":
-        mascaras_recuperadas, seda_usada = actions_hornet(acao)
-
-        if carretel == 8:
-            carretel = 0
-            total_sedas_utilizadas += 8
-            mask_hornet += mascaras_recuperadas
-            if mask_hornet > 5:
-                mask_hornet = 5
+        mascaras_recuperadas = actions_hornet(acao)
+        aux = mask_hornet[0]
+        mask_hornet[0] += mascaras_recuperadas #* Curando
+        if mask_hornet[0] > 5:
+            mask_hornet[0] = 5
+        mask_hornet[1] += mask_hornet[0] - aux #* Vendo quantas máscaras foram geradas
 
     elif acao == "Acerto":
         dano_sofrido = actions_boss(acao)
         
-        mask_hornet -= dano_sofrido
+        mask_hornet[0] -= dano_sofrido
     
-    elif acao == "Duplo Acerto":
+    elif acao == "Acerto Duplo":
         dano_sofrido = actions_boss(acao)
         
-        mask_hornet -= dano_sofrido
+        mask_hornet[0] -= dano_sofrido
     elif acao == "Errou":
         actions_boss(acao)
 
-    seda_desperdicada = total_sedas_recuperadas - total_sedas_utilizadas
+    seda_desperdicada = sistem_carretel[1] - sistem_carretel[2]
 
-    return mask_hornet, mascaras_recuperadas, carretel, seda_desperdicada, vida_chefe, total_sedas_utilizadas, total_sedas_recuperadas
+    return seda_desperdicada, vida_chefe
 
 #Combate
 batalha = True
 hornet_win = False
 ##Status Da Hornet
-mask_hornet = 5
-carretel = 0
-seda_recuperada = seda_desperdicada = mascaras_recuperadas = 0
-total_sedas_recuperadas = total_sedas_utilizadas = 0
+mask_hornet = [5, 0] #* [0] Máscaras da Hornet; [1] Máscaras recuperadas
+sistem_carretel = [0, 0, 0] #* [0] é o carretel de seda; [1] é total de seda geradas; [2] total de seda gasta
+seda_desperdicada = 0
 ##Status Do Chefe
 vida_chefe = 140
 while batalha:
     acao_hornet = str(input())
-    mask_hornet, mascaras_recuperadas, carretel, seda_desperdicada, vida_chefe, total_sedas_utilizadas, total_sedas_recuperadas = sistem_battle(acao_hornet, mascaras_recuperadas, mask_hornet, carretel, seda_recuperada, seda_desperdicada, vida_chefe, total_sedas_recuperadas, total_sedas_utilizadas)
+    seda_desperdicada, vida_chefe = sistem_battle(acao_hornet, vida_chefe)
     
     if vida_chefe > 0:
         acao_boss = str(input())
-        mask_hornet, mascaras_recuperadas, carretel, seda_desperdicada, vida_chefe, total_sedas_utilizadas, total_sedas_recuperadas = sistem_battle(acao_boss, mascaras_recuperadas, mask_hornet, carretel, seda_recuperada, seda_desperdicada, vida_chefe, total_sedas_recuperadas, total_sedas_utilizadas)
+        seda_desperdicada, vida_chefe = sistem_battle(acao_boss, vida_chefe)
 
-    if mask_hornet <= 0: #*Hornet Perde
+    if mask_hornet[0] <= 0: #*Hornet Perde
         batalha = False
         
     if vida_chefe <= 0: #*Hornet Ganha
@@ -112,6 +110,6 @@ while batalha:
         hornet_win = True
 
 if hornet_win:
-    print(f"RESULTADOS DA BATALHA\nMáscaras restantes: {mask_hornet}\nMáscaras recuperadas: {mascaras_recuperadas}\nSeda restante: {carretel}\nSeda desperdiçada: {seda_desperdicada}\n\nHornet: Não cairei tão fácil.")
+    print(f"RESULTADOS DA BATALHA\nMáscaras restantes: {mask_hornet[0]}\nMáscaras recuperadas: {mask_hornet[1]}\nSeda restante: {sistem_carretel[0]}\nSeda desperdiçada: {seda_desperdicada}\n\nHornet: Não cairei tão fácil.")
 else:
     print(f"Hornet: Hm?\nVida Restante: {vida_chefe}")
