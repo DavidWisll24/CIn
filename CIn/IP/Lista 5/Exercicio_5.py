5c8904a90f8c4281eae91d91e19dc2756533e795
dwcf <dwcf@g2c02.cin.ufpe.br>
2026-05-13 09:53:04 -0300

    Q5

--- CIn/IP/Lista 5/Exercicio_5.py
+++ CIn/IP/Lista 5/Exercicio_5.py
@@ -1,8 +1,17 @@
 #A CORRIDA DE KARTS
 
 #Funções Recursivas
-
+def corrida():
+    pass
 
 #INICIO
 condicoes_base = [int(condicao) for condicao in str(input()).split()] #*Cria uma lista com quantas pistas teram [0], a velocidade inicial da Vanellope [1] e a quantidade de glitch [2]
 acoes = [10.0, 0, -10.0] #*Ações possíveis que devem ser realizadas em cada pista nova | [0] -> Acelerar; [1] -> Manter; [1] -> Frear
+
+"""
+Condições parada
+velocidade acima do limite (pista curva)
+velocidade == 0
+
+
+"""
